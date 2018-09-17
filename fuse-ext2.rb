674e675f9f083fa6c80ec3c36b732f48c3a89c5a
Chris Jones <chris.r.jones.1983@gmail.com>
2018-09-16 23:52:33 -0500

    add a formula for fuse-ext2.rb

---
+++ fuse-ext2.rb
@@ -0,0 +1,35 @@
+class Fuseext2 < Formula
+  desc "Fuse-ext2 is a multi OS FUSE module to mount ext2, ext3 and ext4 file system devices and/or images with read write support."
+  homepage "https://github.com/alperakcan/fuse-ext2"
+  url "https://github.com/alperakcan/fuse-ext2"
+  version "0.0.10"
+
+  bottle do
+
+  end
+
+  head do
+    url "git@github.com:alperakcan/fuse-ext2.git", :shallow => false
+
+    depends_on "m4"
+    depends_on "autoconf"
+    depends_on "automake"
+    depends_on "libtool"
+    depends_on "e2fsprogs"
+  end
+
+  depends_on "m4"
+  depends_on "autoconf"
+  depends_on "automake"
+  depends_on "libtool"
+  depends_on "e2fsprogs"
+
+  def install
+    if build.head?
+
+    else
+
+      
+      system "make", "install"
+    end
+end
