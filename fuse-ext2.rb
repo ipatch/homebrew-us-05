0a51c52105c08cd32ce13cc674de3fb1dc64fdec
Chris Jones <chris.r.jones.1983@gmail.com>
2018-09-17 19:22:25 -0500

    add caveat for installing fuse-ext2

--- fuse-ext2.rb
+++ fuse-ext2.rb
@@ -1,9 +1,8 @@
 class FuseExt2 < Formula
   desc "Fuse-ext2 is a multi OS FUSE module to mount ext2, ext3 and ext4 file system devices and/or images with read write support."
-  homepage "https://github.com/alperakcan/fuse-ext2/archive/v0.0.10.tar.gz"
-  version "v0.0.10"
-  url "https://github.com/alperakcan/"
-  sha256 "6414eff0baae099413590ef7109abbc09bbb2605d057ddee92bfc4ce18543080"
+  homepage "https://github.com/alperakcan/"
+  url "https://github.com/alperakcan/fuse-ext2/archive/v0.0.10.tar.gz"
+  sha256 "ad2260df4ccfb8ba9f761c66ea7c3b24bf690eab46e6d562d2d2e5a5f2f76dff"
   head "https://github.com/alperakcan/fuse-ext2.git"
 
   depends_on "pkg-config" => :build
@@ -13,15 +12,29 @@ class FuseExt2 < Formula
   depends_on "libtool" => :build
   depends_on "e2fsprogs" => :build
 
+  conflicts_with "ext2fuse"
+
   def install
-    ENV["LIBTOOL"] = 'glibtool"
-    ENV.prepend_path "PATH", "/usr/local/opt/m4/bin"
-    ENV.prepend_path "PKG_CONFIG_PATH", "/usr/local/lib/pkgconfig"
-    ENV.append_path "PKG_CONFIG_PATH", "/usr/local/opt/e2fsprogs/lib/pkgconfig"
+    ENV.append "LIBS", "-losxfuse"
+    ENV.append "CFLAGS", "-idirafter/usr/local/include/osxfuse/fuse"
+    ENV.append "CFLAGS", "--std=gnu89" if ENV.compiler == :clang
+
+    # Include e2fsprogs headers *after* system headers with -idirafter
+    # as uuid/uuid.h system header is shadowed by e2fsprogs headers
+    ENV.remove "HOMEBREW_INCLUDE_PATHS", Formula["e2fsprogs"].include
+    ENV.append "CFLAGS", "-idirafter" + Formula["e2fsprogs"].include
+
     system "./autogen.sh"
-    ENV["CFLAGS"] = "-idirafter/usr/local/include " + "idirafter/usr/local/include/osxfuse "
-    ENV["LDFLAGS"] = "-L/usr/local/lib " + "-L/usr/local/opt/e2fsprogs/lib "
-    system "./configure"
+    system "./configure", "--prefix=#{prefix}" 
     system "make"
+  end
+
+  def caveats; <<~EOS
+    This formula requires osxfuse to be installed.
+
+    Fuse for macOS can be installed with the below command:
+
+    brew cask install osxfuse
+  EOS
   end
 end
