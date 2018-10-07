97b25b4249b127503acac72113a58189c1149f21
Chris Jones <chris.r.jones.1983@gmail.com>
2018-10-07 00:43:32 -0500

    update `crosstool-ng` to depend on gettext

--- crosstool-ng.rb
+++ crosstool-ng.rb
@@ -27,6 +27,7 @@ class CrosstoolNg < Formula
     depends_on "libtool"
     depends_on "m4"
     depends_on "xz"
+    depends_on "gettext"
   end
 
   depends_on "help2man" => :build
@@ -41,12 +42,15 @@ class CrosstoolNg < Formula
   depends_on "libtool"
   depends_on "m4"
   depends_on "xz"
+  depends_on "gettext"
 
   def install
     if build.head?
       ENV["SED"] = "#{Formula["gnu-sed"].opt_bin}/sed"
       ENV["M4"] = "#{Formula["m4"].opt_bin}/m4"
       ENV["MAKE"] = "/usr/bin/make" # prevent hardcoding make path from superenv
+      # ENV["LDFLAGS"] = "#{Formula["gettext"]}/opt/gettext/lib"
+      # ENV["CPPFLAGS"] = "#{Formula["gettext"]}/opt/gettext/inclue"
 
       system "./bootstrap"
 
