0126c36b9058005a7d6e3bef8873059496e8ec07
Chris Jones <chris.r.jones.1983@gmail.com>
2018-11-22 02:54:01 -0600

    add `odie` log stating OpenSSH can only be built from HEAD

--- openssh.rb
+++ openssh.rb
@@ -1,17 +1,16 @@
 class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools built with LibreSSL"
   homepage "https://www.openssh.com/"
-  # url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.8p1.tar.gz"
-  sha256 "1a484bb15152c183bb2514e112aa30dd34138c3cfb032eee5490a66c507144ca"
-  head "https://anongit.mindrot.org/openssh.git"
-
-  option "--with-lndns", "Build with ldns support"
+  url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-7.9p1.tar.gz"
+  # sha256 "1a484bb15152c183bb2514e112aa30dd34138c3cfb032eee5490a66c507144ca"
+  sha256 "6b4b3ba2253d84ed3771c8050728d597c91cfce898713beb7b64a305b6f11aad"
+  head "https://github.com/openssh/openssh-portable.git"
 
   depends_on "libressl"
   depends_on "ldns" => :optional
   depends_on "pkg-config" => :build if build.with? "ldns"
 
-  # Both these patches are applied by Apple.
+  # Both of these patches are applied by Apple.
   patch do
     url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a74/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
     sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
@@ -46,7 +45,7 @@ class Openssh < Formula
 
       args << "--with-ldns" if build.with? "ldns"
       
-      # system "autoreconf"
+      system "/usr/local/bin/autoreconf"
       system "./configure", *args
       system "make"
       ENV.deparallelize
@@ -60,35 +59,7 @@ class Openssh < Formula
       buildpath.install resource("com.openssh.sshd.sb")
       (etc/"ssh").install "com.openssh.sshd.sb" => "org.openssh.sshd.sb"
     else
-      ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
-
-      # Ensure sandbox profile prefix is correct.
-      # We introduce this issue with patching, it's not an upstream bug.
-      inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
-
-      args = %W[
-      --with-libedit
-      --with-kerberos5
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}/ssh
-      --with-pam
-      --with-ssl-dir=#{Formula["libressl"].opt_prefix}
-      ]
-
-      args << "--with-ldns" if build.with? "ldns"
-
-      system "./configure", *args
-      system "make"
-      ENV.deparallelize
-      system "make", "install"
-
-      # This was removed by upstream with very little announcement and has
-      # potential to break scripts, so recreate it for now.
-      # Debian have done the same thing.
-      bin.install_symlink bin/"ssh" => "slogin"
-
-      buildpath.install resource("com.openssh.sshd.sb")
-      (etc/"ssh").install "com.openssh.sshd.sb" => "org.openssh.sshd.sb"
+      odie "This version of OpenSSH is designed to be built from HEAD and with LibreSSL"
     end
   end
 
