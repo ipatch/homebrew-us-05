d76b542b72e2c34f0aae3687fe3a0b970fb89ac7
Chris Jones <chris.r.jones.1983@gmail.com>
2020-11-05 12:18:06 -0600

    not sure why these shasums keep changing on me

--- Casks/freecad-pre.rb
+++ Casks/freecad-pre.rb
@@ -1,6 +1,6 @@
 cask "freecad-pre" do
   version "0.19_pre,22894"
-  sha256 "a74c87512c845569fd356207be86e71c6691cbe26c7700b792591105c1257586"
+  sha256 "a31643b9816e11ec30d36536ad7ac12cef26d92ed87a9823adeb256cd8a09173"
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.after_comma}-macOS-x86_64-conda.dmg"
