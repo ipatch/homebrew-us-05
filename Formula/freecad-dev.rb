db534a56b3eb19f49b4c5abe03224cadefb793fe
Chris Jones <chris.r.jones.1983@gmail.com>
2020-11-24 21:32:53 -0600

    #coin checkin coin3d project under coin-dev.rb formula file

--- Formula/freecad-dev.rb
+++ Formula/freecad-dev.rb
@@ -23,30 +23,30 @@ class FreecadDev < Formula
   option "with-unsecured-cloud", "Build with self signed certificate support CLOUD module"
   option "with-ninja", "Build using ninja"
 
+  depends_on "boost"
   depends_on "ccache" => :build
   depends_on "cmake" => :build
   depends_on "swig" => :build
   depends_on "freetype"
-  depends_on "boost"
-  depends_on macos: :high_sierra # do not have a sierra box to test formula with
+  depends_on macos: :high_sierra # no sierra box to test
   depends_on "open-mpi"
   depends_on "openblas"
   depends_on "pkg-config"
   depends_on "boost-python3"
-  depends_on "python@3.9"
-  depends_on "qt"
-  depends_on "vtk@8.2"
   depends_on "opencascade"
   depends_on "orocos-kdl"
+  depends_on "python@3.9"
+  depends_on "qt"
   depends_on "freecad/freecad/coin"
   depends_on "freecad/freecad/nglib"
+  depends_on "freecad/freecad/matplotlib"
   depends_on "freecad/freecad/med-file"
   depends_on "freecad/freecad/opencamlib"
-  depends_on "freecad/freecad/shiboken2"
+  depends_on "FreeCAD/freecad/pivy"
   depends_on "freecad/freecad/pyside2"
   depends_on "freecad/freecad/pyside2-tools"
-  depends_on "FreeCAD/freecad/pivy"
-  depends_on "freecad/freecad/matplotlib"
+  depends_on "freecad/freecad/shiboken2"
+  depends_on "vtk@8.2"
   depends_on "webp"
   depends_on "xerces-c"
 
@@ -56,36 +56,20 @@ class FreecadDev < Formula
   end
 
   def install
-    if build.with?("packaging-utils")
-      system "node", "install", "-g", "app_dmg"
-    end
     if (!File.exist?("/usr/local/lib/python3.9/site-packages/six.py"))
       system "pip3", "install", "six"
     end
-    if build.with?("cloud")
-      args << "-DBUILD_CLOUD=1"
-    end
-    if build.with?("unsecured-cloud")
-      args << "-DALLOW_SELF_SIGNED_CERTIFICATE=1"
-    end
-    # args << '-DCMAKE_PREFIX_PATH="' + Formula["qt"].opt_prefix + "/lib/cmake;" + Formula["nglib"].opt_prefix + "/Contents/Resources;" + Formula["vtk@8.2"].opt_prefix + "/lib/cmake;"
 
-    # NOTE: use the latest clang compilers from brew, not Xcode if macOS 10.13, or 10.14
-    ENV["CC"] = Formula["llvm"].opt_bin/"clang"
-    ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
+    # NOTE: latest clang compilers req,Xcode nowork on macOS 10.1{3,4}
+    # ENV["CC"] = Formula["llvm"].opt_bin/"clang"
+    # ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
 
     # NOTE: freecad will not build using Xcode 10.2 on high sierra
-    #
-    # ENV["CC"] = '/Applications/Xcode.10.2.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang'
-    # ENV["CXX"] = '/Applications/Xcode.10.2.1.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang'
-
-    # TODO: TEST 'C++14' with high sierra
     # TODO: fix installation path 
-    #
     # TODO: make condition for high sierra
     # -DBUILD_ENABLE_CXX_STD='C++11'
 
-    args = %W[
+    args = std_cmake_args + %W[
       -Wno-dev
       -Wno-deprecated-declarations
       -DBUILD_ENABLE_CXX_STD='C++17'
@@ -98,18 +82,26 @@ class FreecadDev < Formula
       -DCMAKE_PREFIX_PATH=/usr/local/opt/qt/lib/cmake;/usr/local/opt/nglib/Contents/Resources;
       -DFREECAD_USE_EXTERNAL_KDL=ON
       -DFREECAD_CREATE_MAC_APP=OFF
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
       -DCMAKE_BUILD_TYPE=#{build.with?("debug") ? "Debug" : "Release"}
     ]
 
+    args << "-DALLOW_SELF_SIGNED_CERTIFICATE=1" if build.with? "unsecured-cloud"
+    args << "-DBUILD_CLOUD=1" if build.with? "cloud"
+
+    if build.with?("packaging-utils")
+      system "node", "install", "-g", "app_dmg"
+    end
+
     mkdir "Build" do
       if build.with?("ninja")
-        system "cmake", "-G", "Ninja", *args_travis, ".."
+        system "cmake", "-G", "Ninja", *args, ".."
 
         # TODO: test below make cmd, with `--build` and `.`
         system "make", "-j#{ENV.make_jobs}", "--build", "."
         system "make", "--install", "."
       else
-        system "cmake", "." *std_cmake_args, *args
+        system "cmake", *args, ".."
 
         # NOTE: standard `make install` will fail on 10.14, err, `/usr/local/MacOS/PySide
         # system "make", "--install", "."
