75f786b090505cdc665bf08493155c21f727b64b
Chris Jones <chris.r.jones.1983@gmail.com>
2020-12-23 11:18:16 -0600

    checkin #osxfuse, last publicly available open source version

---
+++ Casks/osxfuse@3.8.3.rb
@@ -0,0 +1,35 @@
+cask 'osxfuse@3.8.3' do
+  version '3.8.3'
+  sha256 '87e507c44c19689beefa3d47dd00ba953254d9e616cb633c1b4343407fe99700'
+
+  # github.com/osxfuse was verified as official when first introduced to the cask
+  url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
+  appcast 'https://github.com/osxfuse/osxfuse/releases.atom'
+  name 'OSXFUSE'
+  homepage 'https://osxfuse.github.io/'
+
+  auto_updates true
+  conflicts_with cask: 'osxfuse-dev'
+
+  pkg "Extras/FUSE for macOS #{version}.pkg",
+      choices: [
+                 'choiceIdentifier' => 'com.github.osxfuse.pkg.MacFUSE',
+                 'choiceAttribute'  => 'selected',
+                 'attributeSetting' => 1,
+               ]
+
+  postflight do
+    set_ownership ['/usr/local/include', '/usr/local/lib']
+  end
+
+  uninstall pkgutil: [
+                       'com.github.osxfuse.pkg.Core',
+                       'com.github.osxfuse.pkg.MacFUSE',
+                       'com.github.osxfuse.pkg.PrefPane',
+                     ],
+            kext:    'com.github.osxfuse.filesystems.osxfusefs'
+
+  caveats do
+    reboot
+  end
+end
