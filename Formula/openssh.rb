ca4f2807925f4c48423ca87a7e83877400123913
Chris Jones <chris.r.jones.1983@gmail.com>
2021-02-03 16:46:39 -0600

    add note about installing openssh from tarball and not git source

--- Formula/openssh.rb
+++ Formula/openssh.rb
@@ -6,8 +6,13 @@ class Openssh < Formula
   version "8.4p1"
   sha256 "5a01d22e407eb1c05ba8a8f7c654d388a13e9f226e4ed33bd38748dafa1d2b24"
   license "SSH-OpenSSH"
-  head "https://github.com/openssh/openssh-portable.git"#  , branch: #"master", shallow: false
-  revision 1
+
+  # NOTE: do not install from head, but rather from from source tarball
+
+  # head "https://github.com/openssh/openssh-portable.git"#  , branch: #"master", shallow: false
+  # head "https://github.com/openssh/openssh-portable.git", branch: "master", shallow: false
+
+  revision 2
 
   livecheck do
     url "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/"
@@ -35,12 +40,24 @@ class Openssh < Formula
     sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
   end
 
+  # Both these patches are applied by Apple.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/1860b0a745f1fe726900974845d1b0dd3c3398d6/openssh/patch-sandbox-darwin.c-apple-sandbox-named-external.diff"
+    sha256 "d886b98f99fd27e3157b02b5b57f3fb49f43fd33806195970d4567f12be66e71"
+  end
+
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/patches/d8b2d8c2612fd251ac6de17bf0cc5174c3aab94c/openssh/patch-sshd.c-apple-sandbox-named-external.diff"
+    sha256 "3505c58bf1e584c8af92d916fe5f3f1899a6b15cc64a00ddece1dc0874b2f78f"
+  end
+
+
   def install
-        # ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+        ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
     # Ensure sandbox profile prefix is correct.
     # We introduce this issue with patching, it's not an upstream bug.
-    # inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
+    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
 
     args = %W[
       --prefix=#{prefix}
