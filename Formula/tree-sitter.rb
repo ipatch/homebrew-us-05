9fa90104d5a2c0d34b1fdcfc5ada8c9a873815a7
Chris Jones <chris.r.jones.1983@gmail.com>
2022-03-07 23:47:48 -0600

    #neovim checkin a forked version of tree-sitter

---
+++ Formula/tree-sitter.rb
@@ -0,0 +1,102 @@
+require "language/node"
+
+class TreeSitter < Formula
+  desc "Parser generator tool and incremental parsing library"
+  homepage "https://tree-sitter.github.io/"
+  url "https://github.com/tree-sitter/tree-sitter/archive/v0.20.6.tar.gz"
+  sha256 "4d37eaef8a402a385998ff9aca3e1043b4a3bba899bceeff27a7178e1165b9de"
+  license "MIT"
+  head "https://github.com/tree-sitter/tree-sitter.git", branch: "master"
+
+  depends_on "ipatch/us-05/emscripten" => [:build, :test]
+  depends_on "ipatch/us-05/node" => [:build, :test]
+  depends_on "rust" => :build
+
+  def install
+    system "make", "AMALGAMATED=1"
+    system "make", "install", "PREFIX=#{prefix}"
+
+    # NOTE: This step needs to be done *before* `cargo install`
+    cd "lib/binding_web" do
+      system "npm", "install", *Language::Node.local_npm_install_args
+    end
+    system "script/build-wasm"
+
+    cd "cli" do
+      system "cargo", "install", *std_cargo_args
+    end
+
+    # Install the wasm module into the prefix.
+    # NOTE: This step needs to be done *after* `cargo install`.
+    %w[tree-sitter.js tree-sitter-web.d.ts tree-sitter.wasm package.json].each do |file|
+      (lib/"binding_web").install "lib/binding_web/#{file}"
+    end
+  end
+
+  test do
+    # a trivial tree-sitter test
+    assert_equal "tree-sitter #{version}", shell_output("#{bin}/tree-sitter --version").strip
+
+    # test `tree-sitter generate`
+    (testpath/"grammar.js").write <<~EOS
+      module.exports = grammar({
+        name: 'YOUR_LANGUAGE_NAME',
+        rules: {
+          source_file: $ => 'hello'
+        }
+      });
+    EOS
+    system bin/"tree-sitter", "generate", "--abi=latest"
+
+    # test `tree-sitter parse`
+    (testpath/"test/corpus/hello.txt").write <<~EOS
+      hello
+    EOS
+    parse_result = shell_output("#{bin}/tree-sitter parse #{testpath}/test/corpus/hello.txt").strip
+    assert_equal("(source_file [0, 0] - [1, 0])", parse_result)
+
+    # test `tree-sitter test`
+    (testpath/"test"/"corpus"/"test_case.txt").write <<~EOS
+      =========
+        hello
+      =========
+      hello
+      ---
+      (source_file)
+    EOS
+    system "#{bin}/tree-sitter", "test"
+
+    (testpath/"test_program.c").write <<~EOS
+      #include <string.h>
+      #include <tree_sitter/api.h>
+      int main(int argc, char* argv[]) {
+        TSParser *parser = ts_parser_new();
+        if (parser == NULL) {
+          return 1;
+        }
+        // Because we have no language libraries installed, we cannot
+        // actually parse a string successfully. But, we can verify
+        // that it can at least be attempted.
+        const char *source_code = "empty";
+        TSTree *tree = ts_parser_parse_string(
+          parser,
+          NULL,
+          source_code,
+          strlen(source_code)
+        );
+        if (tree == NULL) {
+          printf("tree creation failed");
+        }
+        ts_tree_delete(tree);
+        ts_parser_delete(parser);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test_program.c", "-L#{lib}", "-ltree-sitter", "-o", "test_program"
+    assert_equal "tree creation failed", shell_output("./test_program")
+
+    # test `tree-sitter build-wasm`
+    ENV.delete "CPATH"
+    system bin/"tree-sitter", "build-wasm"
+  end
+end
