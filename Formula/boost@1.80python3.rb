96bd5fe579d2df2f21e8139d2ab3be8f39558786
chris <chris.r.jones.1983@gmail.com>
2022-10-24 09:53:41 -0500

    macosvm #mojave update my versioned boost-python3 formula to use boost@1.80

--- Formula/boost@1.80python3.rb
+++ Formula/boost@1.80python3.rb
@@ -1,9 +1,8 @@
-class BoostPython310 < Formula
+class BoostAT180python3 < Formula
   desc "C++ library for C++/Python3 interoperability"
   homepage "https://www.boost.org/"
-  # Please add to synced_versions_formulae.json once version synced with boost
-  url "https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.bz2"
-  sha256 "475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39"
+  url "https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.bz2"
+  sha256 "1e19565d82e43bc59209a168f5ac899d3ba471d55c7610c677d4ccf2c9c500c0"
   license "BSL-1.0"
   head "https://github.com/boostorg/boost.git", branch: "master"
 
@@ -11,11 +10,13 @@ class BoostPython310 < Formula
     formula "boost"
   end
 
-  # keg_only :versioned_formula
-
   depends_on "numpy" => :build
   depends_on "boost"
   depends_on "python@3.10"
+
+  def python3
+    "python3.10"
+  end
 
   def install
     # "layout" should be synchronized with boost
@@ -39,26 +40,26 @@ class BoostPython310 < Formula
     # user-config.jam below.
     inreplace "bootstrap.sh", "using python", "#using python"
 
-    pyver = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
-    py_prefix = Formula["python@3.10"].opt_frameworks/"Python.framework/Versions/#{pyver}"
-    py_prefix = Formula["python@3.10"].opt_prefix if OS.linux?
+    pyver = Language::Python.major_minor_version python3
+    py_prefix = if OS.mac?
+      Formula["python@#{pyver}"].opt_frameworks/"Python.framework/Versions"/pyver
+    else
+      Formula["python@#{pyver}"].opt_prefix
+    end
 
     # Force boost to compile with the desired compiler
-    compiler_text = if OS.mac?
-      "using darwin : : #{ENV.cxx} ;"
-    else
-      "using gcc : : #{ENV.cxx} ;"
-    end
     (buildpath/"user-config.jam").write <<~EOS
-      #{compiler_text}
+      using #{OS.mac? ? "darwin" : "gcc"} : : #{ENV.cxx} ;
       using python : #{pyver}
-                   : python3
+                   : #{python3}
                    : #{py_prefix}/include/python#{pyver}
                    : #{py_prefix}/lib ;
     EOS
 
-    system "./bootstrap.sh", "--prefix=#{prefix}", "--libdir=#{lib}",
-                             "--with-libraries=python", "--with-python=python3",
+    system "./bootstrap.sh", "--prefix=#{prefix}",
+                             "--libdir=#{lib}",
+                             "--with-libraries=python",
+                             "--with-python=#{python3}",
                              "--with-python-root=#{py_prefix}"
 
     system "./b2", "--build-dir=build-python3",
@@ -68,16 +69,10 @@ class BoostPython310 < Formula
                    "python=#{pyver}",
                    *args
 
-    # attempt to make `brew install freecad` happy
-    unless Dir.exist?(opt_prefix+'include')
-      mkdir ('include')
-      File.write('include/weneedit.txt', 'FreeCAD cmake need it.')
-    end
-
-    lib.install Dir["install-python3/lib/*.*"]
-    (lib/"cmake").install Dir["install-python3/lib/cmake/boost_python*"]
-    (lib/"cmake").install Dir["install-python3/lib/cmake/boost_numpy*"]
-    doc.install Dir["libs/python/doc/*"]
+    lib.install buildpath.glob("install-python3/lib/*.*")
+    (lib/"cmake").install buildpath.glob("install-python3/lib/cmake/boost_python*")
+    (lib/"cmake").install buildpath.glob("install-python3/lib/cmake/boost_numpy*")
+    doc.install (buildpath/"libs/python/doc").children
   end
 
   test do
@@ -92,17 +87,17 @@ class BoostPython310 < Formula
       }
     EOS
 
-    pyincludes = shell_output("#{Formula["python@3.10"].opt_bin}/python3-config --includes").chomp.split
-    pylib = shell_output("#{Formula["python@3.10"].opt_bin}/python3-config --ldflags --embed").chomp.split
-    pyver = Language::Python.major_minor_version(Formula["python@3.10"].opt_bin/"python3").to_s.delete(".")
+    pyincludes = shell_output("#{python3}-config --includes").chomp.split
+    pylib = shell_output("#{python3}-config --ldflags --embed").chomp.split
+    pyver = Language::Python.major_minor_version(python3).to_s.delete(".")
 
-    system ENV.cxx, "-shared", "-fPIC", "hello.cpp", "-L#{lib}", "-lboost_python#{pyver}", "-o",
-           "hello.so", *pyincludes, *pylib
+    system ENV.cxx, "-shared", "-fPIC", "hello.cpp", "-L#{lib}", "-lboost_python#{pyver}",
+                    "-o", "hello.so", *pyincludes, *pylib
 
     output = <<~EOS
       import hello
       print(hello.greet())
     EOS
-    assert_match "Hello, world!", pipe_output(Formula["python@3.10"].opt_bin/"python3", output, 0)
+    assert_match "Hello, world!", pipe_output(python3, output, 0)
   end
 end
