b0aa3a31f38a4a2f6b49a8e181ad62ab57251f9a
chris <chris.r.jones.1983@gmail.com>
2024-04-01 12:16:01 -0500

    remove appcast stanza, obsolete

--- Casks/freecad-ipre.rb
+++ Casks/freecad-ipre.rb
@@ -4,7 +4,7 @@ cask "freecad-ipre" do
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.after_comma}-macOS-x86_64-conda.dmg"
-  appcast "https://github.com/freecad/freecad/releases.atom"
+  # appcast "https://github.com/freecad/freecad/releases.atom"
   name "FreeCAD"
   desc "3D parametric modeler"
   homepage "https://freecadweb.org/"
