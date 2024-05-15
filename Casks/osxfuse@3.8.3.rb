6f44f440060089f37707cbbafbe8e2b1eddcdde0
chris <chris.r.jones.1983@gmail.com>
2024-05-15 13:22:52 -0500

    osxfuse: attempt to resolve audit exception [no ci]

--- Casks/osxfuse@3.8.3.rb
+++ Casks/osxfuse@3.8.3.rb
@@ -1,37 +1,41 @@
 cask "osxfuse@3.8.3" do
-  version "3.8.3"
-  sha256 "87e507c44c19689beefa3d47dd00ba953254d9e616cb633c1b4343407fe99700"
+  if OS.mac?
+    version "3.8.3"
+    sha256 "87e507c44c19689beefa3d47dd00ba953254d9e616cb633c1b4343407fe99700"
 
-  # github.com/osxfuse was verified as official when first introduced to the cask
-  url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
-  name "OSXFUSE"
-  homepage "https://osxfuse.github.io/"
+    # github.com/osxfuse was verified as official when first introduced to the cask
+    url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
+      name "OSXFUSE"
+    homepage "https://osxfuse.github.io/"
 
-  auto_updates true
-  conflicts_with cask: "osxfuse-dev"
-  depends_on macos: ">= :mojave"
+    auto_updates true
+    conflicts_with cask: "osxfuse-dev"
+    depends_on macos: ">= :mojave"
 
-  pkg "Extras/FUSE for macOS #{version}.pkg",
+    pkg "Extras/FUSE for macOS #{version}.pkg",
       choices: [
         "choiceIdentifier" => "com.github.osxfuse.pkg.MacFUSE",
         "choiceAttribute"  => "selected",
         "attributeSetting" => 1,
       ]
 
-  postflight do
-    set_ownership ["/usr/local/include", "/usr/local/lib"]
-  end
+    postflight do
+      set_ownership ["/usr/local/include", "/usr/local/lib"]
+    end
 
-  uninstall kext:    "com.github.osxfuse.filesystems.osxfusefs",
-            pkgutil: [
-              "com.github.osxfuse.pkg.Core",
-              "com.github.osxfuse.pkg.MacFUSE",
-              "com.github.osxfuse.pkg.PrefPane",
-            ]
+    uninstall kext:    "com.github.osxfuse.filesystems.osxfusefs",
+      pkgutil: [
+        "com.github.osxfuse.pkg.Core",
+        "com.github.osxfuse.pkg.MacFUSE",
+        "com.github.osxfuse.pkg.PrefPane",
+      ]
 
-  caveats do
-    <<-EOS
-    after installing this cask a system reboot is required
-    EOS
-  end
+    caveats do
+      <<-EOS
+      after installing this cask a system reboot is required
+      EOS
+    end
+    else
+      puts "this cask is only compatible with macos"
+    end
 end
