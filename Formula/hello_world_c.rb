a336e425753ad09ab4fef366a72b17cd3c658e08
Chris <chris.r.jones.1983@gmail.com>
2024-06-14 14:34:05 +0000

    hello_world_c: update 1.1 bottle.

--- Formula/hello_world_c.rb
+++ Formula/hello_world_c.rb
@@ -6,11 +6,9 @@ class HelloWorldC < Formula
   sha256 "742b3b98f6ad8d80396d6be1be32ddf66e06466120954c713f1b6ed373786a10"
 
   bottle do
-    root_url "https://ghcr.io/v2/ipatch/us-05"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "0a8eef58589dc1208c45418505287cc1b096a2093dfaffb55befd3f1f50db065"
-    sha256 cellar: :any_skip_relocation, ventura:      "39982dadc350e8fdaf9f1872b91198d0eeee3f9754f98c6bdff228406f810f18"
-    sha256 cellar: :any_skip_relocation, monterey:     "7ac81989d94223629400f8e729dbd3fb164b63f61a420e3071a05361ed7c08d4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "500ee2ed9e370f07818bd63f67d53f0aa74b1fcd54d6d0dc79352767b5ae6eb4"
+    root_url "https://github.com/ipatch/homebrew-us-05/releases/download/hello_world_c-1.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "cd8921d81403a6c9a6488915a7ba459a5f79468a99a8b8986d87ae29f0e2d5b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "49e21f2389326000feb7d8d515804d351fa80fbac6531498d376abeab8a450f5"
   end
 
   def install
