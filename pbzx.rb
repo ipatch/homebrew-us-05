411a3b16556038e4787e1f4b0d773b7fc1c293c3
chris <chris.r.jones.1983@gmail.com>
2024-12-10 14:20:11 -0600

    pbzx: more scaffolding

--- pbzx.rb
+++ pbzx.rb
@@ -1,28 +1,16 @@
-class Xar < Formula
-  desc "Fork of mackyle/ based on tpoechtrager/ eXtensible ARchiver"
-  homepage "https://github.com/tpoechtrager/xar"
-  url "https://github.com/tpoechtrager/xar/archive/5fa4675419cfec60ac19a9c7f7c2d0e7c831a497.tar.gz"
+class Pbzx < Formula
+  desc "Fork of the pbzx stream parser"
+  homepage "https://github.com/NiklasRosenstein/pbzx"
+  url "https://github.com/NiklasRosenstein/pbzx/archive/bf536e167f2e514866f91d7baa0df1dff5a13711.tar.gz"
   version "dev"
-  sha256 "0ef6618d4b841c16d16ff6c9b0f1cd908f5f785ba08bef50178b293a7d170ff4"
-
-  bottle do
-    root_url "https://github.com/ipatch/homebrew-us-05/releases/download/xar-dev"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1dd22789470e69991ef7ccecb4a3e2a3b16b4dbaa18ed418766a1e425eb0051e"
-  end
+  sha256 "1945d98462d68f5daa565def25d994610d13201e0e8f57c042d9c65835c25478"
 
   # NOTE: ipatch, implement a livecheck
 
-  depends_on "autoconf" => :build
-  depends_on "acl"
-  depends_on "bzip2"
-  depends_on "icu4c"
-  depends_on "libxml2"
-  depends_on "openssl"
-  depends_on "xz"
-  depends_on "zlib"
+  depends_on "xar"
 
   def install
-    cd "xar" do
+    cd "pbzx" do
       system "./autogen.sh"
       system "./configure", "--disable-dependency-tracking",
                             "--prefix=#{prefix}"
@@ -32,6 +20,7 @@ class Xar < Formula
   end
 
   test do
-    assert_match "xar", shell_output("#{bin}/xar --version")
+    system "true"
+    # assert_match "xar", shell_output("#{bin}/xar --version")
   end
 end
