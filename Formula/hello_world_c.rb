a8c0856b19dc334916cf26c9e517c8927f613ae7
Chris <chris.r.jones.1983@gmail.com>
2025-08-20 22:19:54 +0000

    hello_world_c: add 1.1 bottle.

--- Formula/hello_world_c.rb
+++ Formula/hello_world_c.rb
@@ -5,6 +5,13 @@ class HelloWorldC < Formula
   version "1.1"
   sha256 "742b3b98f6ad8d80396d6be1be32ddf66e06466120954c713f1b6ed373786a10"
 
+  bottle do
+    root_url "https://ghcr.io/v2/ipatch/us-05"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:      "6d0387ae8c276ff3ee85b6cbc8e599012de36e7971b5b2de0a332097870b0c77"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "59664fa1f97f7f357a71561a344501bdcd953bcc5bddb54da451d6a54b88c6fe"
+  end
+
   def install
     # compile simple hello world c program
     system ENV.cc, "-o", "hello_world_c", "-std=c99", "-Wall", "C.c"
