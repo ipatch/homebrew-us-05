333c159a462552c3a9b534709e4c969426137f60
chris <chris.r.jones.1983@gmail.com>
2025-11-07 12:07:24 -0600

    xar: add a revision block to test out ci
    Closes #57.
    
    Signed-off-by: Chris <chris.r.jones.1983@gmail.com>

--- Formula/xar.rb
+++ Formula/xar.rb
@@ -4,6 +4,7 @@ class Xar < Formula
   url "https://github.com/tpoechtrager/xar/archive/5fa4675419cfec60ac19a9c7f7c2d0e7c831a497.tar.gz"
   version "dev"
   sha256 "0ef6618d4b841c16d16ff6c9b0f1cd908f5f785ba08bef50178b293a7d170ff4"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/ipatch/us-05"
