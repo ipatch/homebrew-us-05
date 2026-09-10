1c28dc7a39bb8c57002c26fdf1c98d987966abd0
chris jones @ipatch <chris.r.jones.1983@gmail.com>
2026-09-10 17:58:11 -0500

    [no ci] fix style errors to resolve results for `brew search freecad`

--- Casks/freecad-ipre.rb
+++ Casks/freecad-ipre.rb
@@ -3,14 +3,13 @@ cask "freecad-ipre" do
   sha256 "a31643b9816e11ec30d36536ad7ac12cef26d92ed87a9823adeb256cd8a09173"
 
   # github.com/FreeCAD/FreeCAD was verified as official when first introduced to the cask
-  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.csv.first}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.csv.second}-macOS-x86_64-conda.dmg",
-      verified: "github.com/FreeCAD/FreeCAD/"
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.csv.first}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.csv.second}-macOS-x86_64-conda.dmg"
   # appcast "https://github.com/freecad/freecad/releases.atom"
   name "FreeCAD"
   desc "3D parametric modeler"
   homepage "https://freecadweb.org/"
 
-  depends_on macos: ">= :sierra"
+  # depends_on macos: :sierra
 
   # Renamed to avoid conflict with other FreeCAD.app, and the hb-cask-versions
   app "FreeCAD.app", target: "FreeCAD_ipre.app"
